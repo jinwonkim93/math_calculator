37b8709175c876b1e01233d9f9ac5ee0e4eed68c
jw93 <wlsdnjs3131@gmail.com>
2020-01-10 15:26:23 +0900

    a

--- function_calculator.py
+++ function_calculator.py
@@ -34,6 +34,18 @@ class Node(object):
         elif self.op is '/':
             return left / right
 
+class Error(object):
+    def __init__(self,error):
+        self.error = error
+    
+    def __str__(self):
+        return "Error"
+    
+    def __repr__(self):
+        return "Error(Expr)"
+    
+    def eval(self):
+        return self.error
         
 class Empty(object):
     def __repr__(self):
@@ -80,59 +92,54 @@ class ExprTail(Node):
 
 
 class Term(Node):
-    def __init__(self, f, expo, tt):
+    def __init__(self, f, tt):
         super(__class__,self)
         self.f = f
-        self.expo = expo
         self.tt = tt
     
     def eval(self):
-        left = self.expo.eval(self.f.eval())
-        #print(left)
-        #left = -self.expo.eval(left) if left < 0 else self.expo.eval(left)
-        return self.tt.eval(left)
+        return self.tt.eval(self.f.eval())
     
     def __str__(self):
-        return f'{str(self.f)}{str(self.expo)}{str(self.tt)}'
+        return f'{str(self.f)}{str(self.tt)}'
     def __repr__(self):
-        return f'Term({repr(self.f)},{repr(self.expo)},{repr(self.tt)})'
+        return f'Term({repr(self.f)},{repr(self.tt)})'
 
 class TermTail(Node):
-    def __init__(self, op, f, expo, tt):
+    def __init__(self, op, f,  tt):
         super(__class__,self)
         self.op = op
         self.f = f
-        self.expo = expo
         self.tt = tt
     
     def eval(self, left):
-        eval_factor = self.expo.eval(self.f.eval())
-        #eval_factor = -self.expo.eval(eval_factor) if eval_factor < 0 else self.expo.eval(eval_factor)
+        eval_factor = self.f.eval()
         left = self.calc(left, eval_factor)
         return self.tt.eval(left)
     
     def __str__(self):
-        return  f'{str(self.op)}{str(self.f)}{str(self.expo)}{str(self.tt)}'
+        return  f'{str(self.op)}{str(self.f)}{str(self.tt)}'
     def __repr__(self):
-        return f'TermTail({repr(self.op)},{repr(self.f)},{repr(self.expo)},{repr(self.tt)})'
+        return f'TermTail({repr(self.op)},{repr(self.f)},{repr(self.tt)})'
 
 
 class Factor(Node):
-    def __init__(self, e, sign = Empty()):
+    def __init__(self, e, sign = Empty(), expo = Empty()):
         super(__class__,self)
         self.e = e
         self.sign = sign
+        self.expo = expo
 
     def eval(self):
         if self.sign is '-':
-            return -self.e.eval() if isinstance(self.e, (Expr, Variable, Factor)) else -float(self.e)
+            return -self.expo.eval(self.e.eval()) if isinstance(self.e, (Expr, Variable, Factor)) else -self.expo.eval(float(self.e))
         else:
-            return self.e.eval() if isinstance(self.e, (Expr, Variable, Factor)) else float(self.e)
+            return self.expo.eval(self.e.eval()) if isinstance(self.e, (Expr, Variable, Factor)) else self.expo.eval(float(self.e))
     
     def __str__(self):
-        return f'({self.sign}{self.e})' if isinstance(self.e, Expr) else f'{self.sign}{self.e}'
+        return f'({self.sign}{self.e}{self.expo})' if isinstance(self.e, Expr) else f'{self.sign}{self.e}{self.expo}'
     def __repr__(self):
-        return f'Factor({repr(self.sign)},{repr(self.e)})'
+        return f'Factor({repr(self.sign)},{repr(self.e)},{repr(self.expo)})'
 
 class Variable(Node):
     def __init__(self, e):
@@ -140,7 +147,7 @@ class Variable(Node):
         self.e = e
     
     def eval(self):
-        return self.e.eval()
+        return self.e.eval() if isinstance(self.e, (Expr, Variable, AngleF)) else float(self.e)
     
     def __str__(self):
         return str(self.e)
@@ -169,8 +176,8 @@ class Expo(Node):
     def eval(self, left):
         eval_factor = self.expo.eval(self.f.eval())
         return left**eval_factor
-        
-    
+
+            
     def __str__(self):
         if isinstance(self.expo, Empty):
             return f'^({str(self.f)})' if isinstance(self.f, Expr) else f'^{str(self.f)}'
@@ -193,24 +200,30 @@ class Log(Node):
     def __repr__(self):
         return f'Log({repr(self.logarithm)},{repr(self.e)})'
 
+
 from collections.abc import Iterable
 
 class Scanner(object):
     def __init__(self, line):
-        self.tokens = re.findall(r'[-+]|[a-z|A-Z]+|\.?[0-9]+|[*+-/()^]', line) + ['EOL']
+        self.tokens = re.findall(r'[-+]|[a-z|A-Z]+|[0-9]*\.?[0-9]+|[*+-/()^]', line) + ['EOL']
         
     def peak(self):
-        return self.tokens[0]
+        if len(self.tokens):
+            return self.tokens[0]
+        else:
+            raise Exception("Empty Tokens, check Expression")
     
     def shift(self):
-        return self.tokens.pop(0)
-        
+        if len(self.tokens):
+            return self.tokens.pop(0)
+        else:
+            raise Exception("Empty Tokens, check Expression")
     
     def takeIt(self, tokenType = None):
         if tokenType is None or self.isType(tokenType):
             return self.shift()
         else:
-            raise Exception("Expected: %s, Actual: %s" % (tokenType, self.peak()))
+            raise Exception("Expected: %s, Actual: %s" % (tokenType.__name__, self.peak()))
         
     def isType(self, tokenType):
         if callable(tokenType):
@@ -221,97 +234,101 @@ class Scanner(object):
         
         else:
             return False
-
-class Parser(object):
-    def __init__(self, scanner):
-        self.tokens = scanner
-        self.variables = {}
     
     def isDigit(self, value):
+        if value in ['e','pi']: return True
         try:
             float(value)
             return True
         except ValueError:
             return False
+        
 
     def isAlpha(self, value):
+        if value in ['e','pi']: return False
         return value.isalpha()
+
+    def isSpecialNum(self, value):
+        if value == 'e': return E
+        if value == 'pi': return PI
+        else: return value
+class Parser(object):
+    def __init__(self, scanner):
+        self.tokens = scanner
+        self.variables = {}
     
+
+    
+    def getVariables(self, line):
+        for variable in line:
+            self.variables[variable.key] = variable.get()
+            
     def parse(self):
-        e = self.parseExpr()
         try:
+            e = self.parseExpr()
             self.tokens.takeIt(['EOL'])    
             return e
-        except:
-            return 'Error'
+        except Exception as e:
+            return Error(e)
     
     def parseExpr(self):
-        print('Expr')
         t = self.parseTerm()
         et = self.parseExprTail()
         return Expr(t, et)
     
     def parseExprTail(self):
-        print('ExprTail')
         if self.tokens.isType(['+', '-']):
             op = self.tokens.takeIt()
-            print(op)
             t = self.parseTerm()
             et = self.parseExprTail()
             return ExprTail(op,t,et)
-        print('Empty')
         return Empty()
     
     def parseTerm(self):
-        print('Term')
         f = self.parseFactor()
-        expo = self.parseExpo()
         tt = self.parseTermTail()
-        return Term(f, expo, tt)
+        return Term(f, tt)
     
     def parseTermTail(self):
-        print('TermTail')
         if self.tokens.isType(['*', '/']):
             op = self.tokens.takeIt()
-            print(op)
             f = self.parseFactor()
-            expo = self.parseExpo()
             tt = self.parseTermTail()
-            return TermTail(op, f, expo, tt)
-        print('Empty')
+            return TermTail(op, f, tt)
         return Empty()
     
     def parseFactor(self):
-        print('Factor')
         if self.tokens.isType(['(']):
-            print(self.tokens.takeIt())
+            self.tokens.takeIt()
             e = self.parseExpr()
-            print(self.tokens.takeIt())
-            return Factor(e)
+            self.tokens.takeIt()
+            expo = self.parseExpo()
+            return Factor(e, expo = expo)
         elif self.tokens.isType(['+','-']):
             sign = self.tokens.takeIt()
             f = self.parseFactor()
-            return Factor(f, sign = sign)
-        elif self.tokens.isType(isAlpha):
+            expo = self.parseExpo()
+            return Factor(f, sign = sign, expo = expo)
+        elif self.tokens.isType(self.tokens.isAlpha):
             var = self.parseVariable()
-            return Factor(var)
+            expo = self.parseExpo()
+            return Factor(var, expo = expo)
+        elif self.tokens.isType(self.tokens.isDigit):
+            num = self.tokens.isSpecialNum(self.tokens.takeIt())
+            expo = self.parseExpo()
+            return Factor(num, expo = expo)
         else:
-            num = self.tokens.takeIt(self.isDigit)
-            print(num)
-            return Factor(num)
+            raise Exception("Invalid Factor")
             
     def parseExpo(self):
-        print('Expo')
         if self.tokens.isType(['^']):
-            print(self.tokens.takeIt())
+            self.tokens.takeIt()
             f = self.parseFactor()
             expo = self.parseExpo()
             return Expo(f, expo)
-        print('Empty')
         return Empty()
     
     def parseVariable(self):
-        print('Variable')
         if self.tokens.isType(['sin','cos','tan']):
             angleF = self.parseAngleF()
             return Variable(angleF)
@@ -319,28 +336,22 @@ class Parser(object):
             log = self.parseLog()
             return Variable(log)
         else:
-            alpha = self.tokens.takeIt(self.isAlpha)
-            print(alpha)
+            alpha = self.tokens.takeIt(self.tokens.isAlpha)
             return Variable(alpha)
     
     def parseAngleF(self):
-        print('angleF')
-        #별로임
         angleF = self.tokens.takeIt()
-        print(angleF)
-        print(self.tokens.takeIt())
+        self.tokens.takeIt()
         e = self.parseExpr()
-        print(self.tokens.takeIt())
+        self.tokens.takeIt()
         return AngleF(angleF, e)
     
     def parseLog(self):
-        print('Log')
         log = self.tokens.takeIt()
-        print(log)
         logarithm = E
-        if self.tokens.isType(self.isDigit):
+        if self.tokens.isType(self.tokens.isDigit):
             logarithm = self.tokens.takeIt()
-        print(self.tokens.takeIt())
+        self.tokens.takeIt()
         e = self.parseExpr()
-        print(self.tokens.takeIt())            
+        self.tokens.takeIt()         
         return Log(logarithm, e)
